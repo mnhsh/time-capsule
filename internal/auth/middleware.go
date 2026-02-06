388bd3f5ead50048b0b9b61f3ebcde8b42b43b1b
Manash <rinpratim0635@gmail.com>
2026-02-06 16:40:12 +0530

    added cors middleware

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -30,3 +30,17 @@ func WithAuthMiddleware(cfg *config.Config, next http.Handler) http.Handler {
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
