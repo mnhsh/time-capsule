bf03a5e1d5b2c40663f40bb354f39419522d5212
Manash <rinpratim0635@gmail.com>
2026-02-19 13:51:25 +0530

    api logic for get capsules for the authenticated user

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -19,11 +19,11 @@ type API struct {
 	cfg *config.Config
 }
 
-func newAPI(cfg *config.Config) *API {
+func NewAPI(cfg *config.Config) *API {
 	return &API{cfg: cfg}
 }
 
-func (a *API) handlerCreateCapsule(w http.ResponseWriter, r *http.Request) {
+func (a *API) HandlerCreateCapsule(w http.ResponseWriter, r *http.Request) {
 	userID, ok := r.Context().Value(auth.UserIDKey).(uuid.UUID)
 	if !ok {
 		response.RespondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
@@ -75,46 +75,7 @@ func (a *API) handlerCreateCapsule(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func (a *API) handlerGetCapsule(w http.ResponseWriter, r *http.Request) {
-	type Capsule struct {
-		ID         string    `json:"id"`
-		Title      string    `json:"title"`
-		CreatedAt  time.Time `json:"created_at"`
-		UnlockAt   time.Time `json:"unlock_at"`
-		IsUnlocked bool      `json:"is_unlocked"`
-	}
-
-	type CapsulesResponse struct {
-		Capsules []Capsule `json:"capsules"`
-	}
-
-	userID, ok := r.Context().Value(auth.UserIDKey).(uuid.UUID)
-	if !ok {
-		response.RespondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
-		return
-	}
-
-	dbCapsules, err := a.cfg.DB.GetCapsulesByUserID(r.Context(), userID)
-	if err != nil {
-		response.RespondWithError(w, http.StatusInternalServerError, "couldn't get user's capsules", err)
-		return
-	}
-	capsules := make([]Capsule, 0, len(dbCapsules))
-	for _, c := range dbCapsules {
-		capsules = append(capsules, Capsule{
-			ID:         c.ID.String(),
-			Title:      c.Title.String,
-			CreatedAt:  c.CreatedAt,
-			UnlockAt:   c.UnlockAt,
-			IsUnlocked: c.IsUnlocked.Bool,
-		})
-	}
-	response.RespondWithJSON(w, http.StatusOK, CapsulesResponse{
-		Capsules: capsules,
-	})
-}
-
-func (a *API) handlerUsers(w http.ResponseWriter, r *http.Request) {
+func (a *API) HandlerUsers(w http.ResponseWriter, r *http.Request) {
 	type request struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
@@ -150,13 +111,13 @@ func (a *API) handlerUsers(w http.ResponseWriter, r *http.Request) {
 	response.RespondWithJSON(w, http.StatusCreated, user)
 }
 
-func (a *API) handlerLogin(w http.ResponseWriter, r *http.Request) {
+func (a *API) HandlerLogin(w http.ResponseWriter, r *http.Request) {
 	type request struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
 	type res struct {
-		AccessToken  string `json:"access_token"`
+		AccessToken  string `json:"token"`
 		RefreshToken string `json:"refresh_token"`
 		ExpiresIn    int    `json:"expires_in"`
 	}
@@ -214,50 +175,46 @@ func (a *API) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func (a *API) handlerRefreshToken(w http.ResponseWriter, r *http.Request) {
-	type res struct {
-		AccessToken string `json:"access_token"`
+func (a *API) HandlerRefreshToken(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		RefreshToken string `json:"refresh_token"`
 	}
 
-	refreshToken, err := auth.GetBearerToken(r.Header)
+	decoder := json.NewDecoder(r.Body)
+	req := request{}
+	err := decoder.Decode(&req)
 	if err != nil {
-		response.RespondWithError(w, http.StatusBadRequest, "Couldn't find token", err)
+		response.RespondWithError(w, http.StatusInternalServerError, "couldn't decode request", err)
 		return
 	}
 
-	user, err := a.cfg.DB.GetUserByRefreshToken(r.Context(), refreshToken)
+	refreshToken, err := a.cfg.DB.GetRefreshToken(r.Context(), req.RefreshToken)
 	if err != nil {
-		response.RespondWithError(w, http.StatusUnauthorized, "Couldn't get user for refresh token", err)
+		response.RespondWithError(w, http.StatusUnauthorized, "couldn't get user for refresh token", err)
 		return
 	}
 
-	accessToken, err := auth.MakeJwt(
-		user.ID,
-		a.cfg.JWTSecret,
-		time.Minute*15,
-	)
-	if err != nil {
-		response.RespondWithError(w, http.StatusUnauthorized, "Couldn't validate token", err)
+	if refreshToken.ExpiresAt < time.Now() || refreshToken.RevokedAt != nil {
+		response.RespondWithError(w, http.StatusBadRequest, "refresh token expired or revoked", nil)
 		return
 	}
-
-	response.RespondWithJSON(w, http.StatusOK, res{
-		AccessToken: accessToken,
-	})
 }
 
-func (a *API) handlerRevoke(w http.ResponseWriter, r *http.Request) {
-	refreshToken, err := auth.GetBearerToken(r.Header)
+func (a *API) handlerCapsuleRetrieve(w http.ResponseWriter, r *http.Request) {
+	token, err := auth.GetBearerToken(r.Header)
 	if err != nil {
-		response.RespondWithError(w, http.StatusBadRequest, "Couldn't find token", err)
+		response.RespondWithError(w, http.StatusUnauthorized, "couldn't find jwt", err)
 		return
 	}
-
-	err = a.cfg.DB.RevokeRefreshToken(r.Context(), refreshToken)
+	userID, err := auth.ValidateJWT(token, a.cfg.JWTSecret)
 	if err != nil {
-		response.RespondWithError(w, http.StatusInternalServerError, "Couldn't revoke session", err)
+		response.RespondWithError(w, http.StatusUnauthorized, "couldn't validate JWT", err)
 		return
 	}
-
-	w.WriteHeader(http.StatusNoContent)
+	capsule, err := a.cfg.DB.GetCapsulesByUserID(r.Context(), userID)
+	if err != nil {
+		response.RespondWithError(w, http.StatusBadRequest, "couldn't get capsules", err)
+		return
+	}
+	response.RespondWithJSON(w, http.StatusOK, capsule)
 }
